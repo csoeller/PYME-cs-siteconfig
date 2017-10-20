54b596d4fae00f676f8f9c55c0975b3497d3c3bb
Christian Soeller <c.soeller@gmail.com>
2017-10-20 22:39:11 +0100

    add protocols to this repo; generic init_Zyla_n.py with new decorator syntax

---
+++ customprotocols/dark100.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+##################
+# standard488.py
+#
+# Copyright David Baddeley, 2009
+# d.baddeley@auckland.ac.nz
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##################
+
+##################################
+# record 100 dark frames and stop
+#        mostly useful for maps
+##################################
+
+#import all the stuff to make this work
+from PYME.Acquire.protocol import *
+import wx
+
+def stop():
+    wx.CallAfter(MainFrame.pan_spool.OnBStopSpoolingButton, None)
+    
+#define a list of tasks, where T(when, what, *args) creates a new task
+#when is the frame number, what is a function to be called, and *args are any
+#additional arguments
+taskList = [
+T(-1, SetCameraShutter, False),
+T(100, stop),
+]
+
+#optional - metadata entries
+metaData = [
+('Protocol.DarkFrameRange', (0, 20)),
+('Protocol.DataStartsAt', 21)
+]
+
+#must be defined for protocol to be discovered
+PROTOCOL = TaskListProtocol(taskList, metaData)
+PROTOCOL_STACK = ZStackTaskListProtocol(taskList, 20, 30, metaData, randomise = False)
