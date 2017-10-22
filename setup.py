aebb08f5312629b36f544e2f105af829b05135d1
Christian Soeller <c.soeller@gmail.com>
2017-10-22 21:47:33 +0100

    tweaking setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,13 @@ from setuptools import setup
 # you may need setuptools instead of distutils
 
 setup(
-    # basic stuff here
+    name='PymeExeterSiteconfig',
+    version='0.1',
+    description='Site specific files for PYME - Univ. Exeter Soeller Lab',
+    url='http://bitbucket.org/christian_soeller/pyme-exeter-siteconfig',
+    author='Christian Soeller and Lab Members',
+    author_email='c.soeller@gmail.com',
+    license='GPL',
     scripts = [
         'scripts/PYMEConfig.py'
     ]
