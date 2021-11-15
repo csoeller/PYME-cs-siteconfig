74ab0a90f317362ce9c36fff3330b8c879f1c0fa
Christian Soeller <c.soeller@gmail.com>
2021-11-15 17:13:15 +0100

    pyueye based launcher

---
+++ launchers/Bern-PC184/pymeacquire-pyueye.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal
+
+set INITPATH=C:\python-support-files\PYME-exeter-siteconfig\init_scripts
+
+rem ONLY modify the path if needed
+set PATH=c:\ProgramData\Miniconda3\condabin;%PATH%
+rem set PATH=c:\ProgramData\Miniconda3\condabin;C:\Program Files\IDS\uEye\develop\bin;%PATH%
+
+call conda.bat activate pyme-shared
+
+pymeacquire -i %INITPATH%\generic\init_ueye_py_n.py
+
+call conda deactivate
+
+cmd /k
