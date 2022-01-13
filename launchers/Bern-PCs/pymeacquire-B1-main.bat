0c2721d2563dbff430c95f0e41655bf0b4a1d50f
Christian Soeller <c.soeller@gmail.com>
2022-01-13 14:46:36 +0100

    updates for moving over to Dell computer

--- launchers/Bern-PCs/pymeacquire-B1-main.bat
+++ launchers/Bern-PCs/pymeacquire-B1-main.bat
@@ -8,7 +8,7 @@ set PATH=c:\ProgramData\Miniconda3\condabin;C:\Program Files\Andor SDK3;%PATH%
 
 call conda.bat activate pyme-shared
 
-pymeacquire -i %INITPATH%\B1\init_B1_main_n.py
+pymeacquire -i %INITPATH%\B1\init_B1_main_n_Dell.py
 
 call conda deactivate
 
