136ffd8fe0e514410e08bbf24d1d904b539d6913
Christian Soeller <c.soeller@gmail.com>
2022-01-17 08:28:38 +0100

    add launcher for stage testing init file

---
+++ launchers/Bern-PCs/pymeacquire-B1-Zyla-and-z-only.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal
+
+set INITPATH=C:\python-support-files\PYME-cs-siteconfig\init_scripts
+
+rem ONLY modify the path if needed
+set PATH=c:\ProgramData\Miniconda3\condabin;C:\Program Files\Andor SDK3;%PATH%
+
+call conda.bat activate pyme-shared
+
+pymeacquire -i %INITPATH%\B1\init_Zyla_offset_piezo_n.py
+
+call conda deactivate
+
+cmd /k
