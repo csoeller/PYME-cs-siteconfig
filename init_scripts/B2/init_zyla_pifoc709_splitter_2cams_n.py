54f69f2de52501470eb556ee6f62834994fa7b7b
Christian Soeller <c.soeller@gmail.com>
2023-10-26 14:08:53 +0200

    init file for Sona + Zyla on same setup

---
+++ init_scripts/B2/init_zyla_pifoc709_splitter_2cams_n.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python
+
+##################
+# init_TIRF.py
+#
+# Copyright David Baddeley, 2009
+# d.baddeley@auckland.ac.nz
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##################
+from PYME.Acquire.ExecTools import joinBGInit, HWNotPresent, init_gui, init_hardware
+import time
+
+@init_hardware('Andor sCMOS 1')
+def init_scmos1(scope):
+    from PYME.Acquire.Hardware.AndorNeo import AndorZyla
+    cam =  AndorZyla.AndorZyla(0)
+    cam.Init()
+    cam.orientation = dict(rotate=False, flipx=False, flipy=False)
+    cam.DefaultEMGain = 0 #hack to make camera work with standard protocols
+    cam.SetROI(512,512,1024,1024)
+    hdmodes = [ match for match in cam.PixelEncodingForGain.keys() if match.startswith('16-bit')]
+    if len(hdmodes) > 0:
+        cam.SetSimpleGainMode(hdmodes[0])
+
+    if cam.CameraModel.getValue().startswith('ZYLA'):
+        cam.port = 'L100'
+        modelname = 'Zyla'
+    elif cam.CameraModel.getValue().startswith('SONA'):
+        cam.port = 'R100'
+        modelname = 'Sona'
+
+    scope.register_camera(cam, modelname)
+    scope.StatusCallbacks.append(cam.TemperatureStatusText)
+    
+@init_hardware('Andor sCMOS 2')
+def init_scmos2(scope):
+    from PYME.Acquire.Hardware.AndorNeo import AndorZyla
+    cam =  AndorZyla.AndorZyla(1)
+    cam.Init()
+    cam.orientation = dict(rotate=False, flipx=False, flipy=False)
+    cam.DefaultEMGain = 0 #hack to make camera work with standard protocols
+    cam.SetROI(512,512,1024,1024)
+    hdmodes = [ match for match in cam.PixelEncodingForGain.keys() if match.startswith('16-bit')]
+    if len(hdmodes) > 0:
+        cam.SetSimpleGainMode(hdmodes[0])
+
+    if cam.CameraModel.getValue().startswith('ZYLA'):
+        cam.port = 'L100'
+        modelname = 'Zyla'
+    elif cam.CameraModel.getValue().startswith('SONA'):
+        cam.port = 'R100'
+        modelname = 'Sona'
+
+    scope.register_camera(cam, modelname)
+    scope.StatusCallbacks.append(cam.TemperatureStatusText)
+
+@init_gui('Zyla Controls')
+def zyla_controls(MainFrame,scope):
+    from PYME.Acquire.Hardware.AndorNeo import ZylaControlPanel
+    scope.camControls['Zyla'] = ZylaControlPanel.ZylaControl(MainFrame, scope.cameras['Zyla'], scope)
+    MainFrame.camPanels.append((scope.camControls['Zyla'], 'Zyla Properties'))
+
+@init_gui('Sona Controls')
+def sona_controls(MainFrame,scope):
+    from PYME.Acquire.Hardware.AndorNeo import ZylaControlPanel
+    scope.camControls['Sona'] = ZylaControlPanel.ZylaControl(MainFrame, scope.cameras['Sona'], scope)
+    MainFrame.camPanels.append((scope.camControls['Sona'], 'Sona Properties'))
+
+@init_hardware('Z Piezo')
+def zpiezo(scope):
+    from PYME.Acquire.Hardware.Piezos import piezo_e709, offsetPiezoREST
+
+    scope._piFoc = piezo_e709.piezo_e709T('COM11')
+    scope.CleanupFunctions.append(scope._piFoc.close)
+
+    scope.piFoc = offsetPiezoREST.server_class()(scope._piFoc)
+    scope.register_piezo(scope.piFoc, 'z', needCamRestart=True) # David's code has an extra ...,needCamRestart=True)
+
+@init_gui('splitter')
+def ini_splitter(MainFrame,scope):
+    from PYME.Acquire.Hardware import splitter
+
+    scope.splt = splitter.Splitter(MainFrame, scope, scope.cameras['Zyla'],
+                                   flipChan = 0, dichroic = 'T710LPXXR',
+                                   transLocOnCamera = 'Left', flip=False,
+                                   dir='left_right', constrain=False)
+
+@init_gui('Focus Keys')
+def focus_keys(MainFrame, scope):
+    from PYME.Acquire.Hardware import focusKeys
+    fk = focusKeys.FocusKeys(MainFrame, scope.piezos[0])
+    MainFrame.time1.WantNotification.append(fk.refresh)
+
+
+#must be here!!!
+joinBGInit() #wait for anyhting which was being done in a separate thread
+
+#scope.SetCamera('A')
+
+time.sleep(.5)
+scope.initDone = True
