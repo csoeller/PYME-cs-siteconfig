0ef52d0526c42a0a3c25df434258dac9c30155c4
Christian Soeller <c.soeller@gmail.com>
2023-12-12 10:57:01 +0100

    add launchers for CALogging

---
+++ init_scripts/tracking/new drift logging/pymeacquire-drift-tracking_cs-CALog.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+set INITPATH=C:\python-support-files\PYME-cs-siteconfig\init_scripts
+
+rem ONLY modify the path if needed
+set PATH=c:\ProgramData\Miniconda3\condabin;C:\python-support-files\PYME-cs-siteconfig\launchers\bin;%PATH%
+rem set PATH=c:\ProgramData\Miniconda3\condabin;C:\Program Files\IDS\uEye\develop\bin;%PATH%
+
+call conda-activate-chosen-env.bat
+
+cd \python-support-files\pymelogs\driftTracking
+pymeacquire -i %INITPATH%\tracking\init_drift_tracking_cs_CALog.py
+
+call conda deactivate
+
+cmd /k
